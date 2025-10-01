6accebbe0bb7ed87e283f06272347a81d5be0fe0
tsun-chuan123 <cges7901100@gmail.com>
2025-10-01 20:28:13 +0800

    update __init__.py

--- ros2_ws/src/vlpoint/point/__init__.py
+++ ros2_ws/src/vlpoint/point/__init__.py
@@ -1,9 +1,1 @@
-"""
-Lightweight package initializer for `point`.
-
-Avoid importing heavy model classes at import time to ensure services
-like the controller can start without ML dependencies present. Modules
-that need models should import from `point.model` explicitly.
-"""
-
-__all__ = []
+from .model import LlavaLlamaForCausalLM
