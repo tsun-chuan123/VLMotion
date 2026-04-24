b7403cda347dcac50b03c407d5faed5d9276b68c
tsun-chuan123 <cges7901100@gmail.com>
2026-04-24 17:39:41 +0800

    update transformers version

--- ros2_ws/src/vlpoint/point/model/__init__.py
+++ ros2_ws/src/vlpoint/point/model/__init__.py
@@ -1,6 +1,25 @@
+from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+
+# Optional model backends should not block core llama imports.
 try:
-    from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
     from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
+except Exception:
+    LlavaMptForCausalLM = None
+    LlavaMptConfig = None
+
+try:
     from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
-except:
-    pass
+except Exception:
+    LlavaMistralForCausalLM = None
+    LlavaMistralConfig = None
+
+__all__ = [
+    "LlavaLlamaForCausalLM",
+    "LlavaConfig",
+]
+
+if LlavaMptForCausalLM is not None and LlavaMptConfig is not None:
+    __all__.extend(["LlavaMptForCausalLM", "LlavaMptConfig"])
+
+if LlavaMistralForCausalLM is not None and LlavaMistralConfig is not None:
+    __all__.extend(["LlavaMistralForCausalLM", "LlavaMistralConfig"])
